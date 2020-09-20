008d2832799c9da7239297ed797b7fe06d8e5c34
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-09-20 18:58:52 -0500

    Booleans

---
+++ Class2.js
@@ -0,0 +1,22 @@
+// booleans
+
+// var shouldbetrue = true;
+// if (shouldbetrue===false){
+//     console.log("var is true")
+// }
+// else{
+//     console.log("var is false")
+// }
+
+var array = [];
+var hasContent = "hello world";
+var doesNotHaveContent = "";
+var isZero = 0;
+var isNumber = 1;
+var nonAssignedVariable;
+var isNotANumber = NaN;
+var isfalse = false;
+// console.log(Boolean(isfalse));
+
+console.log(null === undefined);
+
