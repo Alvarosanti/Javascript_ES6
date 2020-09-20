d1a4095ea5d86a7f3caee719cf7ecac85ec00dae
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-09-20 15:11:24 -0500

    1st commit

---
+++ Class1.js
@@ -0,0 +1,17 @@
+// numbers
+
+// console.log(10*2)
+// console.log(Math.PI)
+// console.log(parseInt("10")*10);
+// console.log(isNaN("2"));
+// console.log(+("10")*10);
+
+// strings
+
+console.log("Hello world!");
+console.log("Hello world!".length);
+console.log("Hello world!".charAt(2));
+console.log("Hello world!".toUpperCase());
+console.log("Hello world!".toLowerCase());
+
+
