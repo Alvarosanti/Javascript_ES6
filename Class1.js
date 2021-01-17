c71d2219070ebf40a7c85ff2e6641cac7bfed27d
Alvarod12345 <alvarosantisteban56@gmail.com>
2021-01-17 12:33:56 -0500

    update class1

--- Class1.js
+++ Class1.js
@@ -13,5 +13,7 @@ console.log("Hello world!".length);
 console.log("Hello world!".charAt(2));
 console.log("Hello world!".toUpperCase());
 console.log("Hello world!".toLowerCase());
+console.log(typeof );
+
 
 
