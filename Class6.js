8c75e10fc647f42ed531d5831f9e5535567c0049
Alvarod12345 <alvarosantisteban56@gmail.com>
2021-01-25 20:17:50 -0500

    Class6 let n const

---
+++ Class6.js
@@ -0,0 +1,50 @@
+// let const
+
+function sayHi() {
+    var shouldSayHi = true;
+    var reallySayHi = true;
+    if(shouldSayHi === true){
+        var myName = "Chupetin";
+        if(reallySayHi === true){
+            var lastName = "Trujillo"
+        }
+        console.log("Hi" +" "+myName+" "+lastName);
+    }
+}
+// sayHi()
+
+// let
+
+function sayHi2() {
+    var shouldSayHi = true;
+    if(shouldSayHi === true){
+        let myName; 
+        //  let reconoce solo cuando esta en un bloque o en una funcion
+        myName = "Chupetin";
+        // console.log("Hi" +" "+myName);
+            }
+    else{
+        myName = "Trujillo";
+        console.log("Hi" +" "+myName);
+    }
+}
+sayHi2();
+
+// const 
+const myArray = [1,2,3,4];
+// console.log(myArray);
+myArray.push(5);
+// console.log(myArray);
+
+// no se puede reasignar o reemplazar 
+const myObject = { name: "XUPETIN"}
+console.log(myObject);
+myObject.name = "XUPETRUJI";
+console.log(myObject);
+
+
+
+
+
+
+
