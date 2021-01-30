b3b335ba6932e23ec65e01511a64a988c87c7b80
Alvarod12345 <alvarosantisteban56@gmail.com>
2021-01-29 19:25:13 -0500

    Update if statements

--- Class7.js
+++ Class7.js
@@ -1,1 +1,33 @@
 // if statements
+
+// const shouldSayHi = false;
+// if(shouldSayHi === true){console.log("Hi there");}
+
+
+// const numberfive = 5;
+// const stringNumberFive = "5";
+// if(numberfive ===  parseFloat(stringNumberFive)){
+//     console.log(numberfive + parseFloat(stringNumberFive));
+// }
+
+// const myArray = [1,2,3];
+// const myOtherArray = [1,2,3];
+// const myOtherArray2 = myArray;  TRUE
+// console.log(myArray === myOtherArray);
+
+// const myFirstNumber = 123;
+// const mySecondNumber = 121;
+// if(myFirstNumber > mySecondNumber){
+//     console.log("Hi there");
+// }
+
+// const banckAccountBalance = 121;
+// const costOfItem = 120;
+// const tax = 0.5;
+// if(banckAccountBalance >= costOfItem + tax){
+//     alert("You can purchase this item")
+// }
+// else{
+//     alert("You cannot purchase this item")
+// }
+
