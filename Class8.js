a1794a6592630236438d52291bb55f3560cc1a73
Alvarod12345 <alvarosantisteban56@gmail.com>
2021-01-30 19:11:37 -0500

    Class8 functions

---
+++ Class8.js
@@ -0,0 +1,10 @@
+// functions
+// const myName = "Chris";
+function SayHi(username,lastName){
+    console.log("Hi" + " " + username + " " +lastName);
+}
+function SayBye(username){
+    console.log("Byeee" + " " + username);
+}
+SayHi("Jordi","ENP");
+SayBye("Sally");
