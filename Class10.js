94f56f5535f587385214598e849d492f81e597f5
Alvarod12345 <alvarosantisteban56@gmail.com>
2021-02-14 12:15:56 -0500

    Class 10 Do while loops

---
+++ Class10.js
@@ -0,0 +1,62 @@
+// loops while & do while
+
+// let numberOfLoops = 0;
+// while (numberOfLoops < 10){
+//     console.log("I am looping!");
+//     console.log(numberOfLoops);
+//     numberOfLoops += 1;
+// }
+
+
+const names = ["Bob", "Patricio","Arenita"];
+function nameLooper(arr,user){
+    let i = 0;
+    while(i<arr.length){
+        if(arr[i] === user){
+            console.log("YEP");
+            console.log(names[i]);
+            break;
+        }
+        i ++;
+    }
+    console.log(i);
+}
+// nameLooper(names,"Arenita");
+
+
+// do while
+// let shoulRunOnlyOnce = true;
+// let i = 0;
+// do{
+//     console.log("Do Looping!");
+//     i+=1;
+//     if(i === 10){
+//         shoulRunOnlyOnce = false;
+//     }
+// } while(shoulRunOnlyOnce);
+
+
+// let condition = false;
+// do{
+//     const userinput = prompt("Enter q to exit!")
+//     if(userinput === "q"){
+//         condition = false;
+//     }else{
+//         condition = true;
+//     }
+// }while(condition);
+
+let money = 600;
+
+do{
+    CanPurchase();
+}while(money>500);
+
+function CanPurchase(){
+    if(money>500){
+        console.log("Can purchase!");
+        return;
+    }else{
+        console.log("Cannot purchase!");
+    }
+}
