02a60e57be8911ed0f5612747d9c86f02a70183d
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-02-18 20:49:24 -0500

    Class 11 For loops

--- Class11.js
+++ Class11.js
@@ -16,15 +16,69 @@ function iceCreamLooper(arr,type){
 }
 // iceCreamLooper(sabores,'ice')
 
-for(let i = 0; i < 50 ; i += 1){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log('Fizz Buzz');
-    }
-    else if(i % 3 === 0){
-        console.log('Fizz');
-    }
-    else if(i % 5 === 0){
-        console.log('Buzz');
-    }
-    console.log(i);
+// for(let i = 0; i < 50 ; i += 1){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log('Fizz Buzz');
+//     }
+//     else if(i % 3 === 0){
+//         console.log('Fizz');
+//     }
+//     else if(i % 5 === 0){
+//         console.log('Buzz');
+//     }
+//     console.log(i);
+// }
+
+// let x = 0;
+// while(x < 10){
+//      x += 1;
+//      if( x === 5){
+//          console.log('I am being skipped :( ')
+//          continue;
+//         // break;
+//      }
+//      console.log(x + 'I`m not being skipped');
+// }
+
+// a:
+// for(let i= 0; i<10;i+=1){
+//     console.log('Loop :'+i)
+//     b:
+//     for(let a = 0; a<10;a+=1){
+//         console.log('Inner loop :'+a)
+//         if(a===3){
+//             break b;
+//         }
+//         if(i===5){
+//             break a;
+//         }
+//     }
+// }
+
+
+// setTimeout(function(){
+//     console.log('one second has passed');
+// },5000);
+
+function Looper(value){
+    // var j = i;
+    setTimeout(function(){
+        console.log(value);
+    },value * 2000);
 }
+for(var i=0; i < 10; i += 1){
+    Looper(i);
+}
+// Looper();
+
+
+
+for(var i=0; i < 10; i += 1){
+    setTimeout(() => {
+        console.log(i);
+    },1000);
+}
+
+
+
+
