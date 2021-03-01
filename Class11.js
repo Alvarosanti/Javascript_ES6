a22b15dadf3d9e9552cd4f0b34fe99d1200faba2
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-02-28 19:11:25 -0500

    Class 11 For in, for each, for of

--- Class11.js
+++ Class11.js
@@ -10,11 +10,11 @@ const sabores = ['Chocolate', 'Vanilla','strawberry','ice'];
 function iceCreamLooper(arr,type){
     for(var index=0; index < arr.length ; index+=1){
         if(arr[index]=== type){
-            console.log(arr[index] + ' is at position ' + index);
+            console.log(arr[index] + ' is at position ' + (index+1));
         }
     }
 }
-// iceCreamLooper(sabores,'ice')
+// iceCreamLooper(sabores,'Chocolate')
 
 // for(let i = 0; i < 50 ; i += 1){
 //     if(i % 3 === 0 && i % 5 === 0){
@@ -61,24 +61,71 @@ function iceCreamLooper(arr,type){
 // },5000);
 
 function Looper(value){
-    // var j = i;
     setTimeout(function(){
         console.log(value);
-    },value * 2000);
+    },value * 1000);
 }
-for(var i=0; i < 10; i += 1){
-    Looper(i);
-}
+
+// for(var i=0; i < 10; i += 1){
+//     Looper(i);
+// }
 // Looper();
 
 
 
-for(var i=0; i < 10; i += 1){
-    setTimeout(() => {
-        console.log(i);
-    },1000);
+// for(var i=0; i < 10; i += 1){
+//     setTimeout(() => {
+//         console.log(i);
+//     },1000);
+// }
+
+
+// const users ={ 1:'Bob', 2:'Arenita',3:'Don cangrejo' };
+// users.forEach((usr)=>{
+//     console.log(usr);
+// })
+// for in
+// for(let usr in users){
+//     if(users[usr]==='Arenita'){
+//         users[usr]='Patricio';
+//     }
+//     // console.log(users[usr]);
+// }
+
+
+
+// // for each
+// const drinks = ['coffee','sprite','tea','coke','pepsi'];
+// // drinks.forEach((drk)=>{
+// //     console.log(drk);
+// // });
+// for(let drk in drinks){
+//     if(drinks[drk]==='tea')break;    
+//     console.log(drinks[drk]);
+// };
+
+
+// for of
+// console.clear();
+const drinks = ['coffee','sprite','tea','coke','pepsi','Inka cola'];
+const sayhi = 'hello world!';
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+for(let drk of numbers){
+    drk += 10;
+    // console.log(drk);
 }
 
+const incrementox = (arr,incremento) => {
+    for(let drk of arr){
+        drk += incremento;
+        console.log(drk);
+    }   
+};
+// console.log(incrementox(numbers,50));
 
+
+const originalArray = ['Bob','Bob','Arenita','Don cangrejo','Patricio','Plankton'];
+const uniqueNameSet = new Set(originalArray);
+for(const n of uniqueNameSet) console.log(n);
 
 
