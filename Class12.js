0d90bb6c8d256ecf87bcefd148a0df4ed91b0750
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-03-03 22:25:52 -0500

    getelbyid

--- Class12.js
+++ Class12.js
@@ -16,12 +16,45 @@
 // mytag[2].innerText = "Hello, my name is Don cangrejo!"
 
 
-const mytag = document.getElementsByTagName("P");
+// const mytag = document.getElementsByTagName("P");
 
-for(let i = 0; i < mytag.length ; i++){
-    mytag[i].innerHTML = 'Renamed!';
+// for(let i = 0; i < mytag.length ; i++){
+//     mytag[i].innerHTML = 'Renamed!';
+// }
+
+// for(const a of mytag){
+//     a.innerHTML = 'Renamed again!!';
+// }
+
+// const span = document.getElementsByTagName("span");
+// for(const sp of span){
+//     sp.innerHTML = 'Somebody';
+// }
+
+
+// const identifier = document.getElementById('identifier');
+// identifier.innerText = 'We all';
+// identifier.innerHTML = '<h1> We all </h1>'
+// console.log(identifier);
+
+
+// const myAppDiv = document.getElementById('app');
+// const specialWord = myAppDiv.getElementsByTagName('span');
+// const totalwords = document.getElementById('total');
+
+// for(const sp of specialWord){
+//     sp.innerText= 'Testin';
+// }
+
+// totalwords.innerHTML = specialWord.length;
+
+const memberUl = document.getElementById('members');
+const allmemb = document.getElementsByTagName('li');
+
+for(const membs of allmemb){
+    if(membs.innerText === 'Patricio'){
+        membs.innerText = 'Plankton';
+        membs.innerHTML = '<h1>Calamardo</h1>'
+    }
 }
 
-for(const a of mytag){
-    a.innerHTML = 'Renamed again!!';
-}
