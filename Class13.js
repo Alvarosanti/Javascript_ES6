b06364d558fbde11a54b13abffa45c511da5f882
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-03-10 21:05:44 -0500

    preventing default

--- Class13.js
+++ Class13.js
@@ -33,8 +33,49 @@ button.addEventListener('click',logEvent);
 // };
 
 function logEvent(e){
-    button.innerText = ('Pls no more clicks!');
+    button.innerText = e.timeStamp;
+    button.removeEventListener('click',logEvent);
     console.log(e.type);
 }
 
+
+const backgrnd = document.getElementById('background');
+
+backgrnd.addEventListener('mousedown',switchBackground);
+function switchBackground(e){
+    if(e.which ===1 ){
+        backgrnd.classList.toggle('background-color');
+    }
+}
+
+// const div2 = document.getElementById('div2');
+// const query = div2.querySelector('a');
+
+// query.addEventListener('click',function(event){
+//     event.preventDefault();
+//     alert('Not today!');
+// });
+
+// disable right click
+// query.addEventListener('contextmenu',function(event){
+//     event.preventDefault();
+//     alert('Not today!');
+// });
+
+
+const list = document.getElementById('list');
+const myform = document.getElementById('form-id');
+const listUl = document.querySelector('ul');
+
+myform.addEventListener('submit',addToList);
+function addToList(e){
+    e.preventDefault();
+    const myinput = document.getElementById('myinput').value;
+    const newLi = document.createElement('li');
+    newLi.innerText = myinput;
+    listUl.appendChild(newLi);
+    myform.reset();
+    // console.log(newLi.innerText)
+
+}
 
