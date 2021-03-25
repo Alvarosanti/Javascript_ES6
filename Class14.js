4e12af2105b2e7d1becd2abcdd2310d791748b0a
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-03-24 21:20:23 -0500

    Destructuring arrays

---
+++ Class14.js
@@ -0,0 +1,15 @@
+// destructuring
+
+// const myArray = new Array();
+// myArray[0] = 1;
+// console.log(myArray);
+
+function foodFilter(arr,term){
+    return arr.filter((t)=> t === term);
+}
+// const result = foodFilter(['arroz con pollo','chaufa','aji de gallina','lomo saltao'],'arroz con pollo');
+// console.log(result);
+
+const anotherarray = [10,20,30,40,50,60,70];
+
+
