dcb5154972687b8b5e7ad32276ccbd2fd7209b48
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-08-19 00:45:00 -0500

    fetch api update

--- Class18.js
+++ Class18.js
@@ -1,8 +1,35 @@
 // fetch api
 
 // global.fetch = require("https://swapi.dev/api/films/1/")
-const fetch = require("node-fetch");
-fetch("https://swapi.dev/api/films/1/")
-    .then(data => data.json())
-    .then(d => console.log('d:',d))
-    .catch(err => console.log('error:',err));
+
+const myDiv = document.getElementById("planet");
+const myBotton = myDiv.querySelector("button");
+
+myBotton.addEventListener("click", getPlanet)
+
+function getPlanet(){
+    // const fetch = require("node-fetch");
+    const randomNum = (Math.floor(Math.random() * 10)+1)
+    fetch(`https://swapi.dev/api/films/${randomNum}/`)
+        .then(data => data.json())
+        .then(d => populatePlanet(d))
+        .catch(err => console.log('error:',err.message));
+    console.log(randomNum);
+}
+
+function populatePlanet(objplanet){
+    // console.log(title);
+    // const newParagrafh = document.createElement("p");
+    // newParagrafh.innerHTML = title;
+    // myDiv.appendChild(newParagrafh);
+    const {title,episode_id,director,producer} = objplanet;
+    const planetDiv = `
+    <div>
+        <h1>${title}</h1>
+        <p>Episodio numero ${episode_id}</p>
+        <p>Director ${director}</p>
+        <p>Productor ${producer}</p>
+    </div>
+    `
+    myDiv.innerHTML += planetDiv;
+}
